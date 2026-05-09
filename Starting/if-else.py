ce569714ab03f2231614e219f73ce9c772c31a23
Arnab Joyanta Saha <arnabjoyantasaha@gmail.com>
2026-05-09 21:57:18 +0600

    .

---
+++ Starting/if-else.py
@@ -0,0 +1,23 @@
+
+is_student = True
+if is_student:
+    print("You are a student.")
+else:
+    print("You are not a student.")
+# In this code, we have a boolean variable is_student that indicates whether the person is a student or not. We use an if-else statement to check the value of is_student. If it is True, we print "You are a student." Otherwise, we print "You are not a student." In this case, since is_student is set to True, the output will be "You are a student."
+
+
+is_online = False
+if is_online:
+    print("You are online.")
+else:
+    print("You are offline.")
+# In this code, we have a boolean variable is_online that indicates whether the person is online or not. We use an if-else statement to check the value of is_online. If it is True, we print "You are online." Otherwise, we print "You are offline." In this case, since is_online is set to False, the output will be "You are offline."
+
+
+age = 20
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+# In this code, we have an integer variable age that represents a person's age. We use an if-else statement to check if the age is greater than or equal to 18. If it is, we print "You are an adult." Otherwise, we print "You are a minor." In this case, since age is set to 20, the output will be "You are an adult."
