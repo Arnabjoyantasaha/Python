7724565b506d3c096c92144bd64cd2e312b92e85
Arnab Joyanta Saha <arnabjoyantasaha@gmail.com>
2026-05-10 08:51:18 +0600

    .

---
+++ Starting/typecasting.py
@@ -0,0 +1,6 @@
+#typecasting is the process of converting a variable from one type to another. In Python, you can use built-in functions to perform typecasting. Here are some common typecasting functions:
+# int(): Converts a value to an integer.
+# float(): Converts a value to a floating-point number.
+# str(): Converts a value to a string.
+# bool(): Converts a value to a boolean.
+# Example of typecasting:
