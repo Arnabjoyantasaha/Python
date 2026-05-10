6477d0f4871a8cecfacfb44f8218f6d444fdad30
Arnab Joyanta Saha <arnabjoyantasaha@gmail.com>
2026-05-11 02:39:49 +0600

    .

---
+++ Starting/input.py
@@ -0,0 +1,5 @@
+#input() = a function that allows user input data and returns it as a string
+name = input("Enter your name: ")  # Prompt the user to enter their name
+age = input("Enter your age: ")  # Prompt the user to enter their age
+print("Your name is:", name)  # Print the user's name
+print("Your age is:", age)  # Print the user's age
