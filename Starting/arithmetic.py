27d2cec52ebddf61c04c47ba86adf1cb01553adf
Arnab Joyanta Saha <arnabjoyantasaha@gmail.com>
2026-05-11 15:01:07 +0600

    arithmetic operations

---
+++ Starting/arithmetic.py
@@ -0,0 +1,52 @@
+# arithmetic operators & math functions
+
+# arithmetic operators
+
+print(5 + 3)  # addition
+# In this example, we are adding 5 and 3 using the + operator. The result will be the sum of 5 and 3, which is 8.
+print(5 - 2)  # subtraction
+# Here, we are subtracting 2 from 5 using the - operator. The result will be the difference between 5 and 2, which is 3.
+print(4 * 3)  # multiplication
+# In this case, we are multiplying 4 by 3 using the * operator. The result will be the product of 4 and 3, which is 12.
+print(10 / 2)  # division
+# Here, we are dividing 10 by 2 using the / operator. The result will be the quotient of 10 and 2, which is 5.0 (since division in Python results in a float).
+print(10 // 3)  # floor division (returns the quotient without the remainder)
+# In this example, we are performing floor division of 10 by 3 using the // operator. The result will be the quotient of 10 and 3 without the remainder, which is 3.
+print(10 % 3)  # modulus (returns the remainder)
+# Here, we are calculating the modulus of 10 by 3 using the % operator. The result will be the remainder when 10 is divided by 3, which is 1.
+print(2 ** 3)  # exponentiation (2 raised to the power of 3)
+# In this case, we are performing exponentiation by raising 2 to the power of 3 using the ** operator. The result will be 8, since 2 multiplied by itself three times equals 8.
+
+print("New")
+
+x = 2
+
+x += 3  # equivalent to x = x + 3
+# In this example, we are using the += operator to add 3 to the current value of x. This is equivalent to writing x = x + 3. After this operation, the value of x will be updated to 5.
+print(x)  # Output: 5
+
+x -= 1  # equivalent to x = x - 1
+# Here, we are using the -= operator to subtract 1 from the current value of x. This is equivalent to writing x = x - 1. After this operation, the value of x will be updated to 4.
+print(x)  # Output: 4
+
+x *= 2  # equivalent to x = x * 2
+# In this case, we are using the *= operator to multiply the current value of x by 2. This is equivalent to writing x = x * 2. After this operation, the value of x will be updated to 8.
+print(x)  # Output: 8
+
+x /= 4  # equivalent to x = x / 4
+# Here, we are using the /= operator to divide the current value of x by 4. This is equivalent to writing x = x / 4. After this operation, the value of x will be updated to 2.0 (since division in Python results in a float).
+print(x)  # Output: 2.0
+
+x //= 2  # equivalent to x = x // 2
+# In this example, we are using the //= operator to perform floor division on the current value of x by 2. This is equivalent to writing x = x // 2. After this operation, the value of x will be updated to 1.0 (since floor division of 2.0 by 2 results in 1.0).
+print(x)  # Output: 1.0
+
+x %= 2  # equivalent to x = x % 2
+# Here, we are using the %= operator to calculate the modulus of the current value of x by 2. This is equivalent to writing x = x % 2. After this operation, the value of x will be updated to 1.0 (since the remainder when 1.0 is divided by 2 is 1.0).
+print(x)  # Output: 1.0
+
+x **= 3  # equivalent to x = x ** 3
+# In this case, we are using the **= operator to perform exponentiation on the current value of x by raising it to the power of 3. This is equivalent to writing x = x ** 3. After this operation, the value of x will be updated to 1.0 (since 1.0 raised to any power is still 1.0).
+print(x)  # Output: 1.0
+
+
